b6f6d197e83152ebb39eeadb192426b68d7ab5cd
Tolaa1 <tolasesaheed@yahoo.co.uk>
2020-02-13 15:15:15 +0000

    letting-system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LettingManagement::Application.config.secret_token = '439f9db018f42f536490f5adf4192d1e04f8e7e212ee90b29b76d21e20b4b94d9dfb7196a37caa243c8b0df41c48393ac1c48fb68021db4f4729947e8ddd1823'
